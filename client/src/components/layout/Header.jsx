7a7e97f13cf51ad3b37da711d3ffdb3f1bf8e087
Yves Casio <yveschristianpcasio@gmail.com>
2023-11-23 18:51:40 +0800

    feat: Header UI

---
+++ client/src/components/layout/Header.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import "../../assets/css/header.css"
+
+const Header = () => {
+    return (
+        <div id='header'>
+            <input className='search-news' type="search" name="searchNews" id="searchNews" placeholder='search' />
+            <img src={require("../../assets/img/recome-light.png")} alt="Recome Logo Icon" />
+            <div className='register-guest'>
+                <p>Sign In</p>
+                <button>Register</button>
+            </div>
+        </div>
+    )
+}
+
+export default Header
---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import Banner from '../components/layout/Banner'
+import Header from '../components/layout/Header'
+
+const Home = () => {
+    return (
+        <div id='homepage'>
+            <Header />
+            <Banner />
+        </div>
+    )
+}
+
+export default Home
