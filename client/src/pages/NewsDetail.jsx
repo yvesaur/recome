15db64b1db541853a0d644b5e917d907cb0e1ac6
Yves Casio <yveschristianpcasio@gmail.com>
2023-12-17 19:28:34 +0800

    fix: Loader indicator for news detail and related news

--- client/src/pages/NewsDetail.jsx
+++ client/src/pages/NewsDetail.jsx
@@ -3,6 +3,7 @@ import { useLocation, useNavigate, useParams } from 'react-router-dom'
 import Fetch from '../api/Fetch'
 import "../assets/css/pages/newsdetail.css"
 import SelectCategory from '../components/SelectCategory'
+import Loader from '../components/animation/Loader'
 import Footer from '../components/layout/Footer'
 import Header from '../components/layout/Header'
 import { NewsContext } from '../context/NewsContext'
@@ -10,7 +11,7 @@ import { NewsContext } from '../context/NewsContext'
 const NewsDetail = () => {
     const { getUserClick, getUserImpression, isAuth, formatDate } = useContext(NewsContext);
     const [selectedNews, setSelectedNews] = useState();
-    const [relatedNews, setRelatedNews] = useState();
+    const [relatedNews, setRelatedNews] = useState([]);
     const { id } = useParams();
     const navigate = useNavigate();
     const hasCalledGetUserImpression = useRef(false);
@@ -46,10 +47,10 @@ const NewsDetail = () => {
 
     const handleNewsSelect = async (id) => {
         try {
-            navigate(`/news/${id}`);
             const response = await Fetch.get((`/news/${id}`))
             setSelectedNews(response.data.data.news)
             window.location.reload()
+            navigate(`/news/${id}`);
         } catch (error) {
             console.log(error);
         }
@@ -60,7 +61,7 @@ const NewsDetail = () => {
             <Header isDisabled={true} />
             <SelectCategory />
             <div className='selected-news-container'>
-                {selectedNews &&
+                {selectedNews ? (
                     <div className='news-card news-detail-card' key={selectedNews.id} onClick={() => {
 
                     }}>
@@ -73,29 +74,37 @@ const NewsDetail = () => {
                         <img className='selectedNews-img' src={selectedNews.img_url} alt="NEWS THUMBNAIL" />
                         <p className='news-abstract'>{selectedNews.abstract}</p>
                     </div>
-
+                ) : (
+                    <Loader />
+                )
                 }
             </div>
+            {selectedNews && <div className='selected-news-link'><a href={selectedNews.url}>Read more...</a> </div>}
             <h1>Related  News you might like</h1>
             <div className='related-news-container'>
-                {relatedNews && relatedNews.slice(0, 4).map((news) => {
-                    return (
-                        news.id !== id && (
-                            <div className={`news-card related-news`} key={news.id} onClick={() => {
-                                getUserClick(news.id);
-                                handleNewsSelect(news.id);
-                            }}>
-                                <img id='news-thumbnail' src={news.img_url} alt="NEWS THUMBNAIL" />
-                                <p className='related-category'>{news.category}</p>
-                                <p className='related-title'>{news.title}</p>
-                                <div>
-                                    <p className='news-author'>{news.author}</p>
-                                    <p className='news-date'>{formatDate(news.date)}</p>
+                {relatedNews ? (
+                    relatedNews.slice(0, 4).map((news) => {
+                        return (
+                            news.id !== id && (
+                                <div className={`news-card related-news`} key={news.id} onClick={() => {
+                                    getUserClick(news.id);
+                                    handleNewsSelect(news.id);
+                                }}>
+                                    <img id='news-thumbnail' src={news.img_url} alt="NEWS THUMBNAIL" />
+                                    <p className='related-category'>{news.category}</p>
+                                    <p className='related-title'>{news.title}</p>
+                                    <div>
+                                        <p className='news-author'>{news.author}</p>
+                                        <p className='news-date'>{formatDate(news.date)}</p>
+                                    </div>
                                 </div>
-                            </div>
-                        )
-                    );
-                })}
+                            )
+                        );
+                    })
+                ) : (
+                    <Loader />
+                )
+                }
             </div>
             <Footer />
         </div>
