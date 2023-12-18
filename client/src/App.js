224cef1c1fffb13c9ff943fe212744e703903369
Yves Casio <yveschristianpcasio@gmail.com>
2023-12-18 14:24:41 +0800

    refactor: CSS style for mobile

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import "./App.css";
+import "./AppMobile.css";
 
 /* COMPONENTS EXPORTS */
 import { ToastContainer } from "react-toastify";
