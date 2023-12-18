8413a857fe776011751659ee602bb1d33b8a97a4
Yves Casio <yveschristianpcasio@gmail.com>
2023-12-18 15:43:59 +0800

    feat: Homepage mobile responsiveness

--- client/src/components/SelectCategory.jsx
+++ client/src/components/SelectCategory.jsx
@@ -9,18 +9,18 @@ const SelectCategory = () => {
 
     return (
         <nav id='select-category'>
-            <ul>
+            <ul className='category-ul'>
                 <li>
                     <NavLink activeClassName="active" to="/">Home</NavLink>
-                </li>
-                <li>
-                    <NavLink activeClassName="active" to="/latest">Latest</NavLink>
                 </li>
                 {isAuthenticated &&
                     <li>
                         <NavLink activeClassName="active" to="/recommended">Recommended</NavLink>
                     </li>
                 }
+                <li>
+                    <NavLink activeClassName="active" to="/latest">Latest</NavLink>
+                </li>
                 <li>
                     <NavLink activeClassName="active" to="/trending">Trending</NavLink>
                 </li>
