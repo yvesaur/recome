dd21256299e100e09d587be918a017bcb0a706ca
Yves Casio <yveschristianpcasio@gmail.com>
2024-01-13 02:36:38 +0800

    fix: Production Fix

--- client/src/api/Fetch.js
+++ client/src/api/Fetch.js
@@ -1,16 +1,9 @@
 import axios from "axios";
 
-/*
 const baseURL =
   process.env.NODE_ENV === "production"
     ? "https://recome.news/api/v1"
     : "https://recome.news/api/v1";
-*/
-
-const baseURL =
-  process.env.NODE_ENV === "production"
-    ? "http://localhost:5000/api/v1"
-    : "http://localhost:5000/api/v1";
 
 export default axios.create({
   baseURL,
